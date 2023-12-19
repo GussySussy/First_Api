44b0f83d3488e1f871ca00db9680fa08b8aaabed
GussySussy <gourisankar.shaji@gmail.com>
2023-12-19 18:49:05 +0530

    added models and validation

--- resources/item.py
+++ resources/item.py
@@ -1,61 +1,66 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
 from schemas import ItemSchema, ItemUpdateSchema
+from models.item import ItemModel
+from db import db
 
 blp = Blueprint("Items", __name__, description = "Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message = "Item could not be found")
+            item = ItemModel.query.get_or_404(item_id)
+            return item
     
     def delete(self, item_id):
-        try:
-            del items[item_id]  
-            return {"message" : "Item deleted"}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item has been deleted"}
     
     
-    def put(self, item_id):
-        item_data = request.get_json()
-    
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad Request. Ensure 'price' and 'name' is included in the JSON payload.")
-            
-        try:
-            item = items[item_id]
-            item |= item_data
-            
-            return item
-        except KeyError:
-            abort(404, message = "Item could not be found")
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200,ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get(item_id)
+        if item_id:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id,**item_data)
+        
+        db.session.add(item)
+        db.session.commit()
+        
+        return item
 
 
 @blp.route("/item")
 class ItemList(MethodView):
+    
+    @blp.response(200,ItemSchema(many=True))
     def get(self):
-        return {"items" : list(items.values())}
+        return ItemModel.query.all()
     
     @blp.arguments(ItemSchema)
+    @blp.response(200, ItemSchema)
     def post(self, item_data):
-        item_data = request.get_json()
-    
-        if("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
-            abort(400,message = "Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload")
-            
-        for item in items.values():
-            if ( item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
-                abort(400, message="Item already exists")
+        item = ItemModel(**item_data)
         
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="An item with that name already exists")
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the item")
+            
+        db.session.add(item)
+        db.session.commit()
         return item , 201
         
--- schemas.py
+++ schemas.py
@@ -1,10 +1,14 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -13,3 +17,10 @@ class ItemUpdateSchema(Schema):
 class StoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+    
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True,load_only = True)
+    store = fields.Nested(PlainStoreSchema(), dump_only = True)
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.Nested(PlainItemSchema(), dump_only = True)  
